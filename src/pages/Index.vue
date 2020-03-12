64ff83f0213b898e2df6b36db6d7b9a6c6666a3b
JOSE LUCAS ALVES DOS SANTOS <FR042154@catolica.ubec>
2020-03-12 20:02:40 -0300

    swapi

---
+++ src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-card
+    class="my-card text-white"
+    style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+  >
+    <q-card-section
+      v-for="Personagem in Personagens, Planeta in Planetas"
+      :key="Personagem.name"
+    >
+      <div class="text-h6">{{Personagem.name}}</div>
+      <div class="text-subtitle2">{{Planeta.name}}</div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      Personagens: []
+    }
+  },
+  methods: {
+    CarregarPersonagem () {
+      this.$axios.get('https://swapi.co/api/people/')
+        .then(res => {
+          this.Personagens = (res.data.results)
+        })
+    }
+  },
+  beforeMount () {
+    this.CarregarPersonagem()
+  }
+}
+</script>
